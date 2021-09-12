b8d076e7a85a4caabef9c510bc5aa870f9ff7d09
Angus <angussbjohnson@gmail.com>
2021-09-12 15:23:40 +1000

    Remove default hello world on first opening

--- src/components/useBefunge.ts
+++ src/components/useBefunge.ts
@@ -23,10 +23,9 @@ export function useBefunge(
     document.addEventListener("copy", e.onCopy);
     document.addEventListener("paste", e.onPaste);
 
-    // Paste from either url or default "Hello, world!" program
+    // Paste from local storage
     const cookieCode = localStorage.getItem("code");
     if (cookieCode) e.code.set(JSON.parse(cookieCode));
-    else e.onPaste({clipboardData: {getData: (): string => "v>v>v>v>v>v>v               \n5 5 5 4 5 5 9               \n6 7-7-2 7 7+8               \n+ +7+22 + +1*               \n3*9*9*2*9*9*>,,,,,,,,,,,,,,@\n*:*:*:*4*9*:                \n * +3+*+3+ *                \n 5 6+6*6+7 5                \n 2 5 5 5 5 2                \n>^>^>^>^>^>^                \n                            \n                            \n                            \n!dlrow ,olleH               \n"}} as unknown as ClipboardEvent); // prettier-ignore
 
     return (): void => {
       document.removeEventListener("keydown", e.onKeyDown);
